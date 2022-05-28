29ddeb10e184e69d955e90e1acfa37e98ebf5191
trannguyenhan <trannguyenhan01092000@gmail.com>
2022-05-28 08:01:44 +0700

    update constants

--- backend/src/main/java/com/hust/movie_review/common/constant/Constant.java
+++ backend/src/main/java/com/hust/movie_review/common/constant/Constant.java
@@ -3,5 +3,8 @@ package com.hust.movie_review.common.constant;
 public class Constant {
     public static final int SUCCESS_CODE = 200;
     public static final String SUCCESS_MESSAGE = "success";
+    public static String ROLE_MEMBER = "member";
+    public static String ROLE_ADMIN = "admin";
+    public static String ROLE_CRITIC = "critic";
 }
 

93e542b3f2286ac88cf7b8f0e8245eeef0b2b98d
Trung Duc <ntd1312000@gmail.com>
2022-06-14 21:48:18 +0700

    add chức năng quản lý phim

--- vue-admin/src/views/table/index.vue
+++ vue-admin/src/views/table/index.vue
@@ -8,32 +8,38 @@
       fit
       highlight-current-row
     >
-      <el-table-column align="center" label="ID" width="95">
+      <el-table-column align="center" label="ID" width="35">
         <template slot-scope="scope">
           {{ scope.$index }}
         </template>
       </el-table-column>
-      <el-table-column label="Title">
+      <el-table-column label="Tên phim">
         <template slot-scope="scope">
           {{ scope.row.title }}
         </template>
       </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
+      <el-table-column label="Đạo diễn" width="110" align="center">
         <template slot-scope="scope">
           <span>{{ scope.row.author }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
-        <template slot-scope="scope">
-          {{ scope.row.pageviews }}
-        </template>
+      <el-table-column label="Diễn viên" width="110" align="center">
+        <!-- <template slot-scope="scope">
+        </template> -->
       </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
-        <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
-        </template>
+      <el-table-column label="Quốc gia" width="110" align="center">
+        <!-- <template slot-scope="scope">
+        </template> -->
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
+      <el-table-column label="Thể loại" width="80" align="center">
+        <!-- <template slot-scope="scope">
+        </template> -->
+      </el-table-column>
+      <el-table-column label="Đánh giá" width="85" align="center">
+        <!-- <template slot-scope="scope">
+        </template> -->
+      </el-table-column>
+      <el-table-column align="center" prop="created_at" label="Ngày phát hành" width="200">
         <template slot-scope="scope">
           <i class="el-icon-time" />
           <span>{{ scope.row.display_time }}</span>

75912b0ffb8750b66b86865665c2d44296dfb1cb
Trung Duc <ntd1312000@gmail.com>
2022-06-28 05:47:20 +0700

    Add feature quan ly user

--- vue-admin/src/api/user.js
+++ vue-admin/src/api/user.js
@@ -8,16 +8,32 @@ export function login(data) {
   })
 }
 
-export function getInfo(token) {
+export function getInfo() {
   return request({
     url: '/user/me',
-    method: 'get',
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
+    method: 'get'
   })
 }
-
+export function updateUser(params) {
+  return request({
+    url: '/admin/user/update',
+    method: 'post',
+    data: params
+  })
+}
+export function deleteUser(id) {
+  return request({
+    url: `/admin/user/delete/${id}`,
+    method: 'post'
+  })
+}
+export function createUser(params) {
+  return request({
+    url: '/admin/user/create',
+    method: 'post',
+    data: params
+  })
+}
 export function logout() {
   return request({
     url: '/auth/logout',
--- vue-admin/src/views/table/account.vue
+++ vue-admin/src/views/table/account.vue
@@ -15,31 +15,45 @@
       </el-table-column>
       <el-table-column label="Họ và tên" width="220">
         <template slot-scope="scope">
-          {{ scope.row.title }}
+          {{ scope.row.full_name }}
         </template>
       </el-table-column>
       <el-table-column label="Số điện thoại" width="130" align="center">
         <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
+          <span>{{ scope.row.phone_number }}</span>
         </template>
       </el-table-column>
       <el-table-column label="Email" align="center">
-        <!-- <template slot-scope="scope">
-        </template> -->
+        <template slot-scope="scope">
+          <span>{{ scope.row.email }}</span>
+        </template>
       </el-table-column>
       <el-table-column label="Username" width="150" align="center">
-        <!-- <template slot-scope="scope">
-        </template> -->
+        <template slot-scope="scope">
+          <span>{{ scope.row.username }}</span>
+        </template>
       </el-table-column>
       <el-table-column label="Password" width="150" align="center">
-        <!-- <template slot-scope="scope">
-        </template> -->
+        <template slot-scope="scope">
+          <span>{{ scope.row.password }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="Actions" align="center" width="200" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button type="primary" size="mini" @click="handleUpdate(scope.$index)">
+            Edit
+          </el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.$index)">
+            Delete
+          </el-button>
+        </template>
       </el-table-column>
     </el-table>
   </div>
 </template>
-
 <script>
+import { getInfo, updateUser, deleteUser } from '@/api/user'
+
 export default {
   filters: {
     statusFilter(status) {
@@ -54,11 +68,64 @@ export default {
     return {
       list: null,
       user: null,
-      listLoading: true
+      listLoading: true,
+      dialogFormVisible: false,
+      dialogCreate: false
     }
   },
   created() {
     this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.listLoading = true
+      getInfo().then(response => {
+        this.list = response.data
+        if (this.list.length > 0) {
+          this.user = this.list[0]
+        } else {
+          this.user = {
+            name: '',
+            description: ''
+          }
+        }
+        this.listLoading = false
+      })
+    },
+    updateUser(user) {
+      updateUser(user).then(
+        response => {
+          if (response.code === 200) {
+            this.$notify({
+              message: 'Update success',
+              type: 'success'
+            })
+            this.dialogFormVisible = false
+          }
+        }
+      )
+    },
+    handleDelete(index) {
+      deleteUser(
+        this.list[index].id
+      ).then(
+        response => {
+          if (response.code === 200) {
+            this.$notify({
+              message: 'Update success',
+              type: 'success'
+            })
+            this.fetchData()
+          }
+        }
+      )
+    },
+
+    handleUpdate(index) {
+      this.user = this.list[index]
+      this.dialogFormVisible = true
+      this.dialogCreate = false
+    }
   }
 }
 </script>
